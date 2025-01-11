14a2863e57baa73cee72610a87fa1ab1051bdae5
Andersondesign1 <josanderson25@gmail.com>
2025-01-12 00:50:24 +0100

    feat: update Sanity configuration and add Studio page for improved content management

--- sanity.config.js
+++ sanity.config.js
@@ -3,14 +3,10 @@ import { deskTool } from 'sanity/desk'
 import { visionTool } from '@sanity/vision'
 import { schema } from './src/sanity/schemaTypes'
 
-const projectId = process.env.VITE_SANITY_PROJECT_ID
-
 export default defineConfig({
   basePath: '/studio',
   projectId: "3zx1ytic",
   dataset: 'production',
   plugins: [deskTool(), visionTool()],
-  schema: {
-    types: schema.types,
-  },
+  schema,
 })
