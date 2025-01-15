261c4a8beb6111fbb368f8072f78f45ab54c3c80
AndersonDesign1 <josanderson25@gmail.com>
2025-01-15 20:22:14 +0100

    feat: reorder components in Home page for improved layout

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ export default function Home() {
   return (
     <main>
       <Hero />
-      <Experience />
       <Skills />
-      <ProjectSection /> {/* Updated and included ProjectSection */}
+      <ProjectSection /> 
+      <Experience />
     </main>
   );
 }
