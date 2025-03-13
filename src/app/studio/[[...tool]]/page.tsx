0c7dd9f666c42bee85f19667bbc86f6068773767
AndersonDesign1 <josanderson25@gmail.com>
2025-03-13 13:52:49 +0100

    feat: migrate StudioPage component from JavaScript to TypeScript, removing obsolete JSX file

--- src/app/studio/[[...tool]]/page.tsx
+++ src/app/studio/[[...tool]]/page.tsx
@@ -7,11 +7,19 @@
  * https://github.com/sanity-io/next-sanity
  */
 
-'use client'
+"use client"
 
-import { NextStudio } from 'next-sanity/studio'
-import config from '../../../../sanity.config'
+import { NextStudio } from "next-sanity/studio"
+import config from "../../../../sanity.config"
+import type { FC } from "react"
 
-export default function StudioPage() {
+/**
+ * Studio page component that renders the Sanity Studio
+ * @returns The Sanity Studio component
+ */
+const StudioPage: FC = () => {
   return <NextStudio config={config} />
 }
+
+export default StudioPage
+
